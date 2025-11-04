e79df2f29a9c25a473fc752d196da192936627d8
LucasNS1308 <lucas123crc@gmail.com>
2025-11-04 17:42:07 -0300

    alteraçaõ no container final de contato

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const swiper = new Swiper('.mySwiper', {
     nextEl: '.swiper-button-next', // Botão próximo
     prevEl: '.swiper-button-prev', // Botão anterior
   },
-  effect: 'slide', // Tipo de transição (pode usar 'fade' para suavizar)
+  //effect: 'slide', Tipo de transição (pode usar 'fade' para suavizar)
+  effect: 'fade',
   slidesPerView: 1, // Mostra 1 slide por vez
 });
